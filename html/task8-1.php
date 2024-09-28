4a579859ae10260f1350b3c193f0b68740d71f2f
fumiaki1103 <159741296+fumiaki1103@users.noreply.github.com>
2024-09-28 14:39:51 +0900

    first commit

---
+++ html/task8-1.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>okabe fumiaki</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
+    <link href="css/styles1.css" media="screen, projection" rel="stylesheet">
+    <link href="css/reset.css" media="screen, projection" rel="stylesheet">
+</head>
+<body>
+<header>
+<div class="header">
+    <h1>ここには会社名が入ります</h1>
+    <div class="heaer_right_box">
+        <div class="nav_btn heaer_right">
+            <a href="#" class="head_btn01">ボタン01</a>
+            <a href="#" class="head_btn02">ボタン02</a>
+        </div>
+        <div class="nav_btn_sp heaer_right_sp">
+            <a href="#" class="head_btn01">01</a>
+            <a href="#" class="head_btn02">02</a>
+        </div>
+    </div>
+</div>
+<nav class="g-navi">
+    <div class="heaer_bottom">
+        <a href="#">メニュー01</a>
+        <a href="#">メニュー02</a>
+        <a href="#">メニュー03</a>
+    </div>
+</nav>
+</header>
+<div class="mv">
+    <h1><img src="mv.png" alt=""></h1>
+</div>
+<main>
+<section class="con_01">
+    <div class="wrapper">
+        <h1>お問い合わせ</h1>
+    </div>
+    <div class=comp>
+        <h1>送信完了しました。</h1>
+    </div>
+</section>
+</main>
+<footer>
+<section id="foot_01">
+    <div class="wrapper">
+        <div class="foot_01_box">
+            <div class="btn_box">
+                <p>こちらからご購入ください</p>
+                <a href="#" class="foot_btn_01">ネットショップ</a>
+            </div>
+            <div class="btn_box">
+                <p>お気軽にお問い合わせください</p>
+                <a href="#" class="foot_btn_02">お問い合わせ</a>
+            </div>
+        </div>
+    </div>
+</section>
+</footer>
+<section id="foot_02">
+    <div class="wrapper">
+        <ul>
+            <li class="foot_logo">ここには会社名が入ります</li>
+            <li>住所が入ります</li>
+            <li>03-1234-5678</li>
+            <li>営業時間：9:00～18:00</li>
+        </ul>
+    </div>
+</section>
+<section id="foot_03">
+        <div class="foot_03_list">
+            <div class="foot_03_link">
+                <a href="#">リンク01</a>
+            </div>
+            <div class="foot_03_link">
+                <a href="#">リンク02</a>
+            </div>
+            <div class="foot_03_link">
+                <a href="#">リンク03</a>
+            </div>
+            <div class="foot_03_link">
+                <a href="#">リンク04</a>
+            </div>
+            <div class="foot_03_link">
+                <a href="#">リンク05</a>
+            </div>
+            <div class="foot_03_link">
+                <a href="#">リンク06</a>
+                <a href="#">リンク07</a>
+            </div>
+        </div>
+</section>
+<section id="copyright">
+    <div class="wrapper">
+        <p>ここには会社名が入ります©Copyright.</p>
+    </div>
+</section>
+</body>
+</html>
